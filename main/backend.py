63ef689fd50472ea357cd7d312d324b41cf59d0b
Alex Latorre <121249947+frozono9@users.noreply.github.com>
2024-12-15 10:46:47 +0100

    Update backend.py

--- main/backend.py
+++ main/backend.py
@@ -1,14 +1,3 @@
-%pip install joblib
-%pip install streamlit
-%pip install numpy
-%pip install pandas
-%pip install joblib
-%pip install tensorflow
-%pip install matplotlib
-%pip install seaborn
-%pip install scikit-learn
-%pip install shap
-
 import numpy as np
 import joblib
 from tensorflow.keras.models import load_model
